543ca99988d86c180859ac86e29226fdabb8dd13
libaice <libaice147@gmail.com>
2026-04-19 10:58:50 +0800

    feat: complete initial pUSD interactions in script

--- script/BaseScript.s.sol
+++ script/BaseScript.s.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.30;
 import "forge-std/Script.sol";
 
 contract BaseScript is Script {
-    function run() external {
-       
+  
+    function setUp() public virtual {
+        vm.createSelectFork(vm.envString("POLYGON_RPC_URL"));
+
+        uint256 PrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(PrivateKey);
     }
 }
+       
--- script/pusd/PUSDScript.s.sol
+++ script/pusd/PUSDScript.s.sol
@@ -2,9 +2,27 @@
 pragma solidity ^0.8.30;
 
 import "forge-std/Script.sol";
+import {BaseScript} from "../BaseScript.s.sol";
+import {console2} from "forge-std/console2.sol";
+import {CollateralToken} from "ctf-exchange-v2/collateral/CollateralToken.sol";
 
-contract PUSDScript is Script {
+contract PUSDScript is BaseScript {
+
+    address PUSD = 0xC011a7E12a19f7B1f670d46F03B03f3342E82DFB;
     function run() external {
-       
+
+        console2.log(block.number);
+
+        CollateralToken pusd = CollateralToken(PUSD);
+        console2.log(pusd.name());
+        console2.log(pusd.symbol());
+        //USDC
+        console2.log("USDC ",pusd.USDC());
+        //USDCE
+        console2.log("USDCE ",pusd.USDCE());
+        //VAULT
+        console2.log("VAULT ",pusd.VAULT());
+        
+
     }
 }
