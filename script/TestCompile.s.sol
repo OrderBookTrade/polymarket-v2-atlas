2306f1c1e7d9c084a17914caf3be28644544e27d
libaice <libaice147@gmail.com>
2026-04-19 10:34:02 +0800

    build: configure foundry workspace and add test compilation script

---
+++ script/TestCompile.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "neg-risk-ctf-adapter/NegRiskOperator.sol";
+
+contract TestCompile is Script {
+    function run() external {
+       
+    }
+}
